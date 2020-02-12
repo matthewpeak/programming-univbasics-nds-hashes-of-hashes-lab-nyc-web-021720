4ad2b3b7cfff4508e711d9f9fc21bcb9eab7997e
Matthew Peak <matthewpeak@gmail.com>
2020-02-12 19:19:38 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,8 +1,3 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
 #
 # PORTION_1 = {
 #   label: "Kingdom",
