5462fade07fdfa32ef80d8d7c388f5c279004eae
Matthew Peak <matthewpeak@gmail.com>
2020-02-12 19:22:29 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,45 +1,32 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+PORTION_1 = {
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class"
+     }
+   }
+ }
 
-def naming_system
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
+ PORTION_2 = {
+   label: "Order"
+ }
 
-end
+ PORTION_3 = {
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   }
+ }
+
+ PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+ }
+ 
+ def naming_system()
+  PORTION_1[:sub_category][:sub_category][:sub_category]=PORTION_2
+  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category]=PORTION_3
+  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category]=PORTION_4
+  PORTION_1
+ end
